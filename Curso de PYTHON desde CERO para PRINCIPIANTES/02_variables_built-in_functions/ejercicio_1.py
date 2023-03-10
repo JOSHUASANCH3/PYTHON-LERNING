03e79fd350b587b1cdaa494d9efeee003ccce454
MancowithLag <jokayou3@gmail.com>
2023-03-10 12:13:19 -0600

    Ya se realizo el primer ejercicio 1 de 02_variables

--- Curso de PYTHON desde CERO para PRINCIPIANTES/02_variables_built-in_functions/ejercicio_1.py
+++ Curso de PYTHON desde CERO para PRINCIPIANTES/02_variables_built-in_functions/ejercicio_1.py
@@ -1,17 +1,43 @@
-#Ejercicio 2
-"""
-1. Dentro de 30DaysOfPython crea una carpeta llamada day_2. Dentro de esta carpeta crea un archivo llamado [variables.py](http://variables.py/)
-2. Escriba un comentario de python que diga 'Día 2: 30 días de programación en python'
-3. Declarar una variable de nombre y asignarle un valor
-4. Declarar una variable de apellido y asignarle un valor
-5. Declare una variable de nombre completo y asígnele un valor
-6. Declarar una variable de país y asignarle un valor
-7. Declarar una variable de ciudad y asignarle un valor
-8. Declarar una variable de edad y asignarle un valor
-9. Declarar una variable de año y asignarle un valor
-10. Declarar una variable is_married y asignarle un valor
-11. Declarar una variable is_true y asignarle un valor
-12. Declare una variable is_light_on y asígnele un valor
-13. Declarar múltiples variables en una línea
-"""
-print("helloWorld");
+# Ejercicio 1
+# 1. Dentro de 30DaysOfPython crea una carpeta llamada day_2. Dentro de esta carpeta crea un archivo llamado [variables.py](http://variables.py/)
+print("Listo")
+
+# 2. Escriba un comentario de python que diga 'Día 2: 30 días de programación en python'
+print("Día 2: 30 días de programación en python")
+
+# 3. Declarar una variable de nombre y asignarle un valor
+nombre = "Germán"
+
+# 4. Declarar una variable de apellido y asignarle un valor
+apellido = "Sánchez"
+
+# 5. Declare una variable de nombre completo y asígnele un valor
+full_name = "Germán Joshua Sánchez Garay"
+
+# 6. Declarar una variable de país y asignarle un valor
+country = "Nicaragua"
+
+# 7. Declarar una variable de ciudad y asignarle un valor
+city = "Managua"
+
+# 8. Declarar una variable de edad y asignarle un valor
+year_old = "23"
+
+# 9. Declarar una variable de año y asignarle un valor
+year = "1999";
+
+#10. Declarar una variable is_married y asignarle un valor
+is_married = False;
+
+#11. Declarar una variable is_true y asignarle un valor
+is_true = True;
+
+#12. Declare una variable is_light_on y asígnele un valor
+is_ligth_on = True;
+
+#13. Declarar múltiples variables en una línea
+name, last_name, age, is_married_2, country_2 =  "Elizabet", "Rodriguez", 28, False, "Filandia";
+
+#14. Declarar un array con datos existentes
+Array = [nombre, apellido, country, city, year_old, year, is_married, is_ligth_on, is_true];
+print(Array);
--- Curso de PYTHON desde CERO para PRINCIPIANTES/02_variables_built-in_functions/ejercicio_2.py
+++ Curso de PYTHON desde CERO para PRINCIPIANTES/02_variables_built-in_functions/ejercicio_2.py
@@ -1,23 +1,39 @@
 #Ejercicio 2
-"""
-1. Verifique el tipo de datos de todas sus variables usando la función incorporada type()
-2. Usando la función integrada *len()* , encuentre la longitud de su nombre
-3. Compara la longitud de tu nombre y tu apellido
-4. Declare 5 como num_one y 4 como num_two
-    1. Sume num_one y num_two y asigne el valor a un total variable
-    2. Reste num_two de num_one y asigne el valor a una variable diff
-    3. Multiplique num_two y num_one y asigne el valor a un producto variable
-    4. Divide num_one por num_two y asigna el valor a una división variable
-    5. Use la división de módulo para encontrar num_two dividido por num_one y asigne el valor a un residuo 
-				variable
-    6. Calcula num_one a la potencia de num_two y asigna el valor a una variable exp
-    7. Encuentra la división de piso de num_one por num_two y asigna el valor a una variable floor_division
-5. El radio de un círculo es de 30 metros.
-    1. Calcule el área de un círculo y asigne el valor a una variable con el nombre de *area_of_circle*
-    2. Calcule la circunferencia de un círculo y asigne el valor a un nombre de variable de *circum_of_circle*
-    3. Tome el radio como entrada del usuario y calcule el área.
-6. Use la función de entrada integrada para obtener el nombre, el apellido, el país y la edad de un usuario y 
-		almacene el valor en sus nombres de variables correspondientes
-7. Ejecute la ayuda ('palabras clave') en el shell de Python o en su archivo para verificar las palabras reservadas 
-		o las palabras clave de Python
-"""
+#1. Verifique el tipo de datos de todas sus variables usando la función incorporada type()
+first_name = "Germán";       # str
+last_name = "Sánchez";       # str
+country = "Nicaragua";       # str
+city= 'Helsinki';            # str
+age = 250;                   # int, it is not my real age, don't worry about it
+    #Confirmando los tipos de datos
+print(type('Asabeneh'));     # str
+print(type(first_name));     # str
+print(type(250));            # int
+print(type(age));            #int
+
+#2. Usando la función integrada *len()* , encuentre la longitud de su nombre
+print("Cantidad del nombre: ",len(first_name)); #Cantidad de letras
+print("Cantidad del apellido: ",len(last_name));
+
+#3. Compara la longitud de tu nombre y tu apellido
+if(len(first_name) > len(last_name)){ #comparacion de quien tiene mas letras
+    print("Mi primer nombre es mayor que mi apellido");    
+}else{
+    print("Mi apellido es mayor que mi nombre");
+}
+
+#4. Declare 5 como num_one y 4 como num_two
+    num_one, num_two = 5, 4;
+    #1. Sume num_one y num_two y asigne el valor a un total variable
+    #2. Reste num_two de num_one y asigne el valor a una variable diff
+    #3. Multiplique num_two y num_one y asigne el valor a un producto variable
+    #4. Divide num_one por num_two y asigna el valor a una división variable
+    #5. Use la división de módulo para encontrar num_two dividido por num_one y asigne el valor a un residuo variable
+    #6. Calcula num_one a la potencia de num_two y asigna el valor a una variable exp
+    #7. Encuentra la división de piso de num_one por num_two y asigna el valor a una variable floor_division
+#5. El radio de un círculo es de 30 metros.
+    #1. Calcule el área de un círculo y asigne el valor a una variable con el nombre de *area_of_circle*
+    #2. Calcule la circunferencia de un círculo y asigne el valor a un nombre de variable de *circum_of_circle*
+    #3. Tome el radio como entrada del usuario y calcule el área.
+#6. Use la función de entrada integrada para obtener el nombre, el apellido, el país y la edad de un usuario y almacene el valor en sus nombres de variables correspondientes
+#7. Ejecute la ayuda ('palabras clave') en el shell de Python o en su archivo para verificar las palabras reservadas o las palabras clave de Python
