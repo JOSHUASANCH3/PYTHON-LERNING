1c7b142ead0d17dc529d8c1f51086e1c16335d3a
MancowithLag <jokayou3@gmail.com>
2023-03-25 18:34:03 -0600

    Terminando la 05_list de python 'Practicar mas'

--- Curso de PYTHON desde CERO para PRINCIPIANTES/05_list/05_list.py
+++ Curso de PYTHON desde CERO para PRINCIPIANTES/05_list/05_list.py
@@ -179,3 +179,106 @@ print(fruits)       # ['banana', 'orange', 'mango']
 fruits.pop(0)
 print(fruits)       # ['orange', 'mango']
 
+#?Removing Items Using Del
+    #The del keyword removes the specified index and it can also be used to delete items 
+    # within index range. It can also delete the list completely
+fruits = ['banana', 'orange', 'mango', 'lemon', 'kiwi', 'lime']
+del fruits[0]
+print(fruits)       # ['orange', 'mango', 'lemon', 'kiwi', 'lime']
+del fruits[1]
+print(fruits)       # ['orange', 'lemon', 'kiwi', 'lime']
+del fruits[1:3]     # this deletes items between given indexes, so it does not delete the item with index 3!
+print(fruits)       # ['orange', 'lime']
+del fruits
+print(fruits)       # This should give: NameError: name 'fruits' is not defined
+
+#?Clearing List Items
+    #The clear() method empties the list:
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits.clear()
+print(fruits)       # []
+
+#?Copying a List
+    #It is possible to copy a list by reassigning it to a new variable in the following way: list2 = list1. 
+    # Now, list2 is a reference of list1, any changes we make in list2 will also modify the original, list1. 
+    # But there are lots of case in which we do not like to modify the original instead we like to have a 
+    # different copy. One of way of avoiding the problem above is using copy().
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits_copy = fruits.copy()
+print(fruits_copy)       # ['banana', 'orange', 'mango', 'lemon']
+
+#?Joining Lists
+    #There are several ways to join, or concatenate, two or more lists in Python.
+    #Plus Operator (+)
+positive_numbers = [1, 2, 3, 4, 5]
+zero = [0]
+negative_numbers = [-5,-4,-3,-2,-1]
+integers = negative_numbers + zero + positive_numbers
+print(integers) # [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+fruits = ['banana', 'orange', 'mango', 'lemon']
+vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot']
+fruits_and_vegetables = fruits + vegetables
+print(fruits_and_vegetables ) # ['banana', 'orange', 'mango', 'lemon', 'Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot']
+
+    #Joining using extend() method The extend() method allows to append list in a list. See the example below.
+num1 = [0, 1, 2, 3]
+num2= [4, 5, 6]
+num1.extend(num2)
+print('Numbers:', num1) # Numbers: [0, 1, 2, 3, 4, 5, 6]
+negative_numbers = [-5,-4,-3,-2,-1]
+positive_numbers = [1, 2, 3,4,5]
+zero = [0]
+
+negative_numbers.extend(zero)
+negative_numbers.extend(positive_numbers)
+print('Integers:', negative_numbers) # Integers: [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+fruits = ['banana', 'orange', 'mango', 'lemon']
+vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot']
+fruits.extend(vegetables)
+print('Fruits and vegetables:', fruits ) # Fruits and vegetables: ['banana', 'orange', 'mango', 'lemon', 'Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot']
+
+#?Counting Items in a List
+    #The count() method returns the number of times an item appears in a list:
+fruits = ['banana', 'orange', 'mango', 'lemon']
+print(fruits.count('orange'))   # 1
+ages = [22, 19, 24, 25, 26, 24, 25, 24]
+print(ages.count(24))           # 3
+
+#?Finding Index of an Item
+    #The index() method returns the index of an item in the list:
+fruits = ['banana', 'orange', 'mango', 'lemon']
+print(fruits.index('orange'))   # 1
+ages = [22, 19, 24, 25, 26, 24, 25, 24]
+print(ages.index(24))           # 2, the first occurrence
+
+#?Reversing a List
+    #The reverse() method reverses the order of a list.
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits.reverse()
+print(fruits) # ['lemon', 'mango', 'orange', 'banana']
+ages = [22, 19, 24, 25, 26, 24, 25, 24]
+ages.reverse()
+print(ages) # [24, 25, 24, 26, 25, 24, 19, 22]
+
+#?Sorting List Items
+    #sort(): this method modifies the original list
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits.sort()
+print(fruits)             # sorted in alphabetical order, ['banana', 'lemon', 'mango', 'orange']
+fruits.sort(reverse=True)
+print(fruits) # ['orange', 'mango', 'lemon', 'banana']
+ages = [22, 19, 24, 25, 26, 24, 25, 24]
+ages.sort()
+print(ages) #  [19, 22, 24, 24, 24, 25, 25, 26]
+
+ages.sort(reverse=True)
+print(ages) #  [26, 25, 25, 24, 24, 24, 22, 19]
+
+    #sorted(): returns the ordered list without modifying the original list Example:
+fruits = ['banana', 'orange', 'mango', 'lemon']
+print(sorted(fruits))   # ['banana', 'lemon', 'mango', 'orange']
+# Reverse order
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits = sorted(fruits,reverse=True)
+print(fruits)     # ['orange', 'mango', 'lemon', 'banana']
+
---
+++ Curso de PYTHON desde CERO para PRINCIPIANTES/05_list/ejercicio2_day5.py
@@ -0,0 +1,15 @@
+"""
+1. The following is a list of 10 students ages:
+
+`ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]`
+
+- Sort the list and find the min and max age
+- Add the min age and the max age again to the list
+- Find the median age (one middle item or two middle items divided by two)
+- Find the average age (sum of all items divided by their number )
+- Find the range of the ages (max minus min)
+- Compare the value of (min - average) and (max - average), use *abs()* method
+1. Find the middle country(ies) in the [countries list](https://github.com/Asabeneh/30-Days-Of-Python/tree/master/data/countries.py)
+2. Divide the countries list into two equal lists if it is even if not one more country for the first half.
+3. ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']. Unpack the first three countries and the rest as scandic countries.
+"""
