8648d317a2798035617d755c3cc35d8f0eaac0b2
JOSHUASANCH3 <jokayou3@gmail.com>
2023-04-12 14:00:08 -0600

    Update 07_sets and begginig 08_dictionary

---
+++ Curso de PYTHON desde CERO para PRINCIPIANTES/08_dictionary/exercise_1.py
@@ -0,0 +1,12 @@
+#💻 Exercises: Day 8
+#Create an empty dictionary called dog
+#Add name, color, breed, legs, age to the dog dictionary
+#Create a student dictionary and add first_name, last_name, gender, age, marital status, skills, country, city and address as keys for the dictionary
+#Get the length of the student dictionary
+#Get the value of skills and check the data type, it should be a list
+#Modify the skills values by adding one or two skills
+#Get the dictionary keys as a list
+#Get the dictionary values as a list
+#Change the dictionary to a list of tuples using items() method
+#Delete one of the items in the dictionary
+#Delete one of the dictionaries
