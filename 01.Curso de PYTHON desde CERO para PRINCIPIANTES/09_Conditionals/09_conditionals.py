c8070ae85031ad812d93109bebadd63aba5c7ab0
JOSHUASANCH3 <jokayou3@gmail.com>
2023-06-21 23:10:52 -0600

    09_Conditions Base terminado

---
+++ 01.Curso de PYTHON desde CERO para PRINCIPIANTES/09_Conditionals/09_conditionals.py
@@ -0,0 +1,97 @@
+"""
+#?Conditionals
+By default, statements in Python script are executed sequentially from top to bottom. 
+If the processing logic require so, the sequential flow of execution can be altered in two way:
+
+    Conditional execution: a block of one or more statements will be executed if a certain expression is true
+    Repetitive execution: a block of one or more statements will be repetitively executed as 
+    long as a certain expression is true. In this section, we will cover if, else, elif statements. 
+    The comparison and logical operators we learned in previous sections will be useful here.
+
+#?If Condition
+In python and other programming languages the key word if is used to check if a condition is true and to execute 
+the block code. Remember the indentation after the colon.
+
+#!Example
+a = 3
+if a > 0:
+    print('A is a positive number')
+# A is a positive number
+
+As you can see in the example above, 3 is greater than 0. The condition was true and the block code was executed. 
+However, if the condition is false, we do not see the result. In order to see the result of the falsy condition, 
+we should have another block, which is going to be else.
+
+#?If Else
+If condition is true the first block will be executed, if not the else condition will run.
+
+#!Example
+a = 3
+if a < 0:
+    print('A is a negative number')
+else:
+    print('A is a positive number')
+
+The condition above proves false, therefore the else block was executed. How about if our condition is more than two? We could use _ elif_.
+
+#?If Elif Else
+In our daily life, we make decisions on daily basis. We make decisions not by checking one or two conditions 
+but multiple conditions. As similar to life, programming is also full of conditions. We use elif when we have 
+multiple conditions.
+
+#!Example
+a = 0
+if a > 0:
+    print('A is a positive number')
+elif a < 0:
+    print('A is a negative number')
+else:
+    print('A is zero')
+
+#?Short Hand
+#! syntax
+code if condition else code
+
+a = 3
+print('A is positive') if a > 0 else print('A is negative') # first condition met, 'A is positive' will be printed
+
+#?Nested Conditions
+Conditions can be nested
+
+#!Example
+a = 0
+if a > 0:
+    if a % 2 == 0:
+        print('A is a positive and even integer')
+    else:
+        print('A is a positive number')
+elif a == 0:
+    print('A is zero')
+else:
+    print('A is a negative number')
+
+-> We can avoid writing nested condition by using logical operator and.
+
+#?If Condition and Logical Operators
+#!Example
+a = 0
+if a > 0 and a % 2 == 0:
+        print('A is an even and positive integer')
+elif a > 0 and a % 2 !=  0:
+     print('A is a positive integer')
+elif a == 0:
+    print('A is zero')
+else:
+    print('A is negative')
+
+#?If and Or Logical Operators
+#!Example
+user = 'James'
+access_level = 3
+if user == 'admin' or access_level >= 4:
+        print('Access granted!')
+else:
+    print('Access denied!')
+
+
+"""
---
+++ 01.Curso de PYTHON desde CERO para PRINCIPIANTES/09_Conditionals/exercise_1.py
@@ -0,0 +1,26 @@
+"""
+#?💻 Exercises 1: Day 9
+1.Get user input using input(“Enter your age: ”). If user is 18 or older, give feedback: 
+You are old enough to drive. If below 18 give feedback to wait for the missing amount of years. Output:
+
+Enter your age: 30
+You are old enough to learn to drive.
+Output:
+Enter your age: 15
+You need 3 more years to learn to drive.
+
+2.Compare the values of my_age and your_age using if … else. Who is older (me or you)? 
+Use input(“Enter your age: ”) to get the age as input. You can use a nested condition 
+to print 'year' for 1 year difference in age, 'years' for bigger differences, and a custom text 
+if my_age = your_age. Output:
+
+Enter your age: 30
+You are 5 years older than me.
+
+3.Get two numbers from the user using input prompt. If a is greater than b return a is greater than b, 
+if a is less b return a is smaller than b, else a is equal to b. Output:
+
+Enter number one: 4
+Enter number two: 3
+4 is greater than 3
+"""
