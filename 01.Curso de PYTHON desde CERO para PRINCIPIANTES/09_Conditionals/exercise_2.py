b332743014d9303adc8b7db621e49afefa1cc7a3
JOSHUASANCH3 <jokayou3@gmail.com>
2023-09-12 13:55:08 -0600

    Finish both exercise 2 amd 3 of 09_condicionals

--- 01.Curso de PYTHON desde CERO para PRINCIPIANTES/09_Conditionals/exercise_2.py
+++ 01.Curso de PYTHON desde CERO para PRINCIPIANTES/09_Conditionals/exercise_2.py
@@ -1,22 +1,78 @@
+    #?💻 Exercises 2: Day 9
 """
-#?💻 Exercises 2: Day 9
-1.Write a code which gives grade to students according to theirs scores:
+#? 1.Write a code which gives grade to students according to theirs scores:
 
-80-100, A
+90-100, A
 70-89, B
 60-69, C
 50-59, D
 0-49, F
-
-2.Check if the season is Autumn, Winter, Spring or Summer. If the user input is: September, October or November, 
-the season is Autumn. December, January or February, the season is Winter. March, April or May, the season 
-is Spring June, July or August, the season is Summer
-
-3.The following list contains some fruits:
-
-fruits = ['banana', 'orange', 'mango', 'lemon']
-
-->  If a fruit doesn't exist in the list add the fruit to the list and print the modified list. 
-    If the fruit exists print('That fruit already exist in the list')
+"""
+scores_students = input("Ingresive your score");
+if(scores_students >= 90 && scores_students =< 100):
+    print('Your score is "A"');
+elif(scores_students >= 70 && scores_students =< 89):
+    print('Your score is "B"');
+elif(scores_students >= 60 && scores_students =< 69):
+    print('Your score is "C"');
+elif(scores_students >= 50 && scores_students =< 59):
+    print('Your score is "D"');
+elif(scores_students >= 0 && scores_students =< 49):
+    print('Your score is "F"');
 
 """
+#? 2.Check if the season is Autumn, Winter, Spring or Summer. If the user input is: 
+#   September, October or November, the season is Autumn. 
+#   December, January or February, the season is Winter. 
+#   March, April or May, the season is Spring. 
+#   June, July or August, the season is Summer.
+"""
+# Define lists of months for each season
+autumn_months = ["september", "october", "november"]
+winter_months = ["december", "january", "february"]
+spring_months = ["march", "april", "may"]
+summer_months = ["june", "july", "august"]
+
+# Get the month from the user
+month = input("Enter a month: ").lower()
+
+# Determine the season
+if month in autumn_months:
+    season = "Autumn"
+elif month in winter_months:
+    season = "Winter"
+elif month in spring_months:
+    season = "Spring"
+elif month in summer_months:
+    season = "Summer"
+else:
+    season = "Unknown"
+
+# Display the result
+if season != "Unknown":
+    print(f"The season for {month.capitalize()} is {season}.")
+else:
+    print("Invalid input. Please enter a valid month.")
+
+"""
+#? 3.The following list contains some fruits:
+fruits = ['banana', 'orange', 'mango', 'lemon']
+->  If a fruit doesn't exist in the list add the fruit to the list and print the modified list. 
+    If the fruit exists print('That fruit already exist in the list')
+"""
+# List of fruits
+fruits = ['banana', 'orange', 'mango', 'lemon']
+
+# Ask the user for a fruit
+fruit = input("Enter a fruit: ").lower()
+
+# Check if the fruit is already in the list
+if fruit in fruits:
+    print(f"{fruit.capitalize()} already exists in the list.")
+else:
+    # Add the fruit to the list
+    fruits.append(fruit)
+    print(f"{fruit.capitalize()} has been added to the list.")
+
+# Print the modified list of fruits
+print("Updated list of fruits:", fruits)
--- 01.Curso de PYTHON desde CERO para PRINCIPIANTES/09_Conditionals/exercise_3.py
+++ 01.Curso de PYTHON desde CERO para PRINCIPIANTES/09_Conditionals/exercise_3.py
@@ -19,6 +19,44 @@ Here we have a person dictionary. Feel free to modify it!
  * Check if the person dictionary has skills key, if so check if the person has 'Python' skill and print out the result.
  * If a person skills has only JavaScript and React, print('He is a front end developer'), if the person skills has Node, Python, MongoDB, print('He is a backend developer'), if the person skills has React, Node and MongoDB, Print('He is a fullstack developer'), else print('unknown title') - for more accurate results more conditions can be nested!
  * If the person is married and if he lives in Finland, print the information in the following format:
-
- 
 """
+# Person dictionary
+person = {
+    'first_name': 'Joshua',
+    'last_name': 'Sanchez',
+    'age': 24,
+    'country': 'Nicaragua',
+    'is_married': True,
+    'skills': ['Python', 'MySQL', 'Node.js', 'Figma', 'HTML', 'CSS'],
+    'address': {
+        'street': 'Space street',
+        'zipcode': '02210'
+    }
+}
+
+# Task 1: Check if the person dictionary has 'skills' key and print the middle skill
+if 'skills' in person:
+    skills_list = person['skills']
+    if len(skills_list) % 2 == 1:
+        middle_skill = skills_list[len(skills_list) // 2]
+        print(f"The middle skill is: {middle_skill}")
+
+# Task 2: Check if the person dictionary has 'skills' key and if 'Python' is one of the skills
+if 'skills' in person and 'Python' in person['skills']:
+    print("The person has Python skill.")
+
+# Task 3: Determine the developer title based on skills
+if 'skills' in person:
+    skills = person['skills']
+    if 'Node' in skills and 'Python' in skills and 'MongoDB' in skills:
+        print("He is a backend developer")
+    elif 'JavaScript' in skills and 'React' in skills:
+        print("He is a front-end developer")
+    elif 'React' in skills and 'Node' in skills and 'MongoDB' in skills:
+        print("He is a fullstack developer")
+    else:
+        print("Unknown title")
+
+# Task 4: Print information if married and lives in Finland
+if person['is_married'] and person['country'] == 'Finland':
+    print(f"{person['first_name']} {person['last_name']} is married and lives in Finland.")
