c71e8ae34a07c23a459ae7cf076333c7f4f6d73c
JOSHUASANCH3 <jokayou3@gmail.com>
2023-09-12 16:51:07 -0600

    Update 12/9/2023

--- 01.Curso de PYTHON desde CERO para PRINCIPIANTES/10_loops/exercise_1.py
+++ 01.Curso de PYTHON desde CERO para PRINCIPIANTES/10_loops/exercise_1.py
@@ -4,3 +4,7 @@ Iterate 0 to 10 using for loop, do the same using while loop.
 Iterate 10 to 0 using for loop, do the same using while loop.
 Write a loop that makes seven calls to print(), so we get on the output the following triangle:
 """
+count = "0";
+while (count < 10):
+    print("");
+    coount =+ 1;
