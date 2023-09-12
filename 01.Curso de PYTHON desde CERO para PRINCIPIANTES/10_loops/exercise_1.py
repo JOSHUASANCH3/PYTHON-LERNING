157e6cc077fdf2887513a3c92c15297e054b466f
JOSHUASANCH3 <jokayou3@gmail.com>
2023-09-12 15:57:39 -0600

    Fixing up someone files

---
+++ 01.Curso de PYTHON desde CERO para PRINCIPIANTES/10_loops/exercise_1.py
@@ -0,0 +1,6 @@
+ #? Exercise_1
+"""
+Iterate 0 to 10 using for loop, do the same using while loop.
+Iterate 10 to 0 using for loop, do the same using while loop.
+Write a loop that makes seven calls to print(), so we get on the output the following triangle:
+"""
