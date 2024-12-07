80f199bffa5a23e029072a24042f9ca0b59e3056
JOSHUASANCH3 <jokayou3@gmail.com>
2024-12-06 20:15:40 -0600

    Se actualizo el readme del primer curso

---
+++ 01.Curso de PYTHON desde CERO para PRINCIPIANTES/09_Conditionals/practica_0.py
@@ -0,0 +1,7 @@
+##condicionals
+my_condition = False
+if my_condition: ## Es lo mismo que if my_condition == True:
+    print("se ejecuta la condicion del if")
+    
+print("La ejecucion continua")
+ 
--- 01.Curso de PYTHON desde CERO para PRINCIPIANTES/10_loops/exercise_1.py
+++ 01.Curso de PYTHON desde CERO para PRINCIPIANTES/10_loops/exercise_1.py
@@ -1,20 +1,12 @@
- #? Exercise_1
-#Iterate 0 to 10 using for loop, do the same using while loop.
-#?foor loop
-for i in range(11):
-    print(i)
-#?while loop
-counter = 0
-while counter <= 10:
-    print(counter)
-    counter += 1
-#Iterate 10 to 0 using for loop, do the same using while loop.
-#?foor loop
-for i in range(10, -1, -1):
-    print(i)
-#?while loop
-counter = 10;
-while counter >=10;
-    print(counter);
-    counter -= 1;
-#Write a loop that makes seven calls to print(), so we get on the output the following triangle:
+##Practica de bucles
+ 
+ ## While
+ 
+my_condition = 0
+ while my_condition < 10:
+     print(my_condition)
+     my_condition += 1
+else:
+    print("Mi condicion es mayor o igual a 10")
+     
+print("La ejecucion continua")
