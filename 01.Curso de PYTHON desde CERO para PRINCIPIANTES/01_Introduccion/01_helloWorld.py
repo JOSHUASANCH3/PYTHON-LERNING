dc5fdf995ef7820e0731b20a648ea057374980d6
JOSHUASANCH3 <jokayou3@gmail.com>
2025-01-06 22:52:06 -0600

    Prueba SSHKEY

--- 01.Curso de PYTHON desde CERO para PRINCIPIANTES/01_Introduccion/01_helloWorld.py
+++ 01.Curso de PYTHON desde CERO para PRINCIPIANTES/01_Introduccion/01_helloWorld.py
@@ -29,3 +29,4 @@ print(type([1, 2, 3]))   # List
 print(type({'name':'Asabeneh'})) # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Set
 print(type((9.8, 3.14, 2.7))) +   # Tuples
+print("Hola Mundo")##Se agrega una nueva linea
