be4ffdc736ff21902e4d4ba7f4b8504372d55911
JOSHUASANCH3 <jokayou3@gmail.com>
2025-01-10 13:57:53 -0600

    Terminando el modulo HighOrderFunctions Day50

--- 02.Curso de PYTHON desde CERO para INTERMEDIO/14_higher_order_functions/14_higher_order_functions.py
+++ 02.Curso de PYTHON desde CERO para INTERMEDIO/14_higher_order_functions/14_higher_order_functions.py
@@ -23,5 +23,47 @@ def sum_ten(original_value):
     return add
 
 
-add_closure = sum_ten(5)
+add_closure = sum_ten(1)
 print(add_closure(5))
+sum_ten(5)(1)
+
+### BLUIT-IN HIGH ORDER FUNCTIONS ###
+numbers = [1, 2, 3, 4, 5]
+
+
+## MAP
+numbers = [1, 2, 3, 4, 5]
+
+
+def multiply_two(numbers):
+    return numbers + 2
+
+
+print(list(map(multiply_two, numbers)))
+print(list(map(lambda number: number * 2, numbers)))
+
+
+## FILTER
+numbers = [1, 2, 3, 4, 5]
+
+
+def filter_greater_than_ten(number):
+    if number > 10:
+        return True
+    return False
+
+
+print(list(filter(filter_greater_than_ten, numbers)))
+print(list(filter(lambda number: number > 10, numbers)))
+
+## Reduce
+numbers = [1, 2, 3, 4, 5]
+
+
+def sum_two_values(first_value, second_value):
+    print(first_value)
+    print(second_value)
+    return first_value + second_value
+
+
+print(reduce(sum_two_values, numbers))
